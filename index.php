0bc452c62be92f24b03d9e0423c6dcff94a171ea
n4495 <np00004@mix.wvu.edu>
2021-02-04 21:53:38 -0500

    init web dirs and index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<main>
+	<link rel="stylesheet" href="css/about.css">
+
+	<div class="about">
+		<h1>About Us!<h1>
+		<p>Stick Emporium is a world class purveyor of sticks. Please contact us
+			for all your stick needs.<p>
+	</div>
+
+	<div class="row">
+		<div class="column">
+			<div class="card">	
+				<img src="images/1.jpg">
+				<div class="container">
+					<h2>CEO</h2>
+					<p>Big Stick</p>
+					<p class="email">bigStick@StickEmporium.com</p>
+				</div>
+			</div>
+		</div>	
+
+		<div class="column">
+			<div class="card">	
+				<img src="images/2.jpg">
+				<div class="container">
+					<h2>Stick Manager</h2>
+					<p>Cinnamon Stick</p>
+					<p class="email">cstick@StickEmporium.com</p>
+				</div>
+			</div>
+		</div>	
+
+		<div class="column">
+			<div class="card">	
+				<img src="images/3.jpg">
+				<div class="container">
+					<h2>Stick Logistics</h2>
+					<p>Sticko</p>
+					<p class="email">sticko@StickEmporium.com</p>
+				</div>
+			</div>
+		</div>	
+
+		<div class="column">
+			<div class="card">	
+				<img src="images/4.jpg">
+				<div class="container">
+					<h2>Stick Assayer</h2>
+					<p>StickBob</p>
+					<p class="email">StickBob@StickEmporium.com</p>
+				</div>
+			</div>
+		</div>	
+
+	</div>
+
+
+<a href="https://github.com/n4495/cs230_spring2021" class="stickhub">Branch us on StickHub!</a>
+
+
+</main>
