18031611a6df1b9a34044227369351c66bbf9b65
np4495 <np00004@mix.wvu.edu>
2021-03-11 20:05:23 -0500

    added gallery

--- includes/header.php
+++ includes/header.php
@@ -46,6 +46,10 @@ session_start();
 
                     <li class="nav-item">
                         <a class="nav-link" href="profile.php">Profile</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin.php">Admin</a>
                     </li>';
                 }
                     else{
